7e38e2febc95e1ed25e4c3e55d4659c964df55b0
Pedro Garcias <pedro.garcias@vision-box.com>
2021-12-02 11:11:34 +0000

    Fix folder name and warning

--- src/Challenge/Program.cs
+++ src/Challenge/Program.cs
@@ -9,11 +9,12 @@ while (true)
         Console.WriteLine("Enter pokmen map directions");
         var userDirectionsInput = Console.ReadLine();
 
-        mapEngine.DoMovement(userDirectionsInput);
-        var capturesNumber = mapEngine.GetCapturedPokemons();
-
-        Console.WriteLine($"Number of catched pokemons is {capturesNumber}");
-       
+        if (userDirectionsInput != null)
+        {
+            mapEngine.DoMovement(userDirectionsInput);
+            var capturesNumber = mapEngine.GetCapturedPokemons();
+            Console.WriteLine($"Number of catched pokemons is {capturesNumber}");
+        }
     }
     catch (InvalidOperationException ex)
     {
@@ -22,7 +23,7 @@ while (true)
     catch (Exception ex)
     {
         Console.WriteLine(ex.Message);
-        
+
     }
     finally
     {
